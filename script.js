9583733095a1a1d2c19825774d6c656c8e3d6b57
engmohakam-alt <eng.mohakam@gmail.com>
2026-07-25 04:29:11 +0300

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+//=============================
+// Mobile Menu
+//==============================
+
+const menuBtn=document.querySelector(".menu-btn");
+
+const menu=document.querySelector(".menu");
+
+menuBtn.addEventListener("click",()=>{
+
+menu.classList.toggle("show");
+
+});
+
+//==============================
+// Sticky Navbar
+//==============================
+
+window.addEventListener("scroll",()=>{
+
+const navbar=document.querySelector(".navbar");
+
+navbar.classList.toggle("sticky",window.scrollY>60);
+
+});
+
+//==============================
+// Scroll Animation
+//==============================
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("active");
+
+}
+
+});
+
+});
+
+document.querySelectorAll("section").forEach(sec=>{
+
+observer.observe(sec);
+
+});
+
 
